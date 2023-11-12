72ce54a515a24445f9130048c342f1506012de23
Anar Safarov <anarsferov@gmail.com>
2023-11-12 21:58:35 +0400

    add: design system

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Text } from '@chakra-ui/react';
+
+import theme from './theme/chakraTheme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <h1>Avian messaging</h1>
+  <ChakraProvider theme={theme}>
+    <Text color="violet-1" fontSize="xl">
+      Avian messaging
+    </Text>
   </ChakraProvider>
 );
