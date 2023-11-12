adcf89c654d9d606a021ad982d31b16f02abaf38
Anar Safarov <anarsferov@gmail.com>
2023-11-12 20:12:48 +0400

    basic configuration completed

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ChakraProvider>
+    <h1>Avian messaging</h1>
+  </ChakraProvider>
+);
